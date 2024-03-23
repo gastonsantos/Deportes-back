9b7c6a24239c7ca514230f3aad2d90851f1eca41
Gaston Santos <gastons525@gmail.com>
2024-03-23 11:47:12 -0300

    Cambio de Tablas de Db

---
+++ Deportes.Infra/Database/FichaRepository/FichaRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deportes.Infra.Database.FichaRepository
+{
+    public class FichaRepository
+    {
+    }
+}
