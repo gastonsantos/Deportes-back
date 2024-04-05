3aaf751dc5f4a7a34e84a604b4f4dc6c92810a11
Gaston Santos <gastons525@gmail.com>
2024-04-05 14:10:29 -0300

    agrego

---
+++ Deportes.Servicio/Servicios/EventoServices/EventoServices.cs
@@ -0,0 +1,39 @@
+﻿using Deportes.Modelo.EventoModel;
+using Deportes.Servicio.Interfaces.IEvento;
+using Deportes.Servicio.Interfaces.IFichas;
+using Deportes.Servicio.Interfaces.IUsuario;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deportes.Servicio.Servicios.EventoServices;
+
+public class EventoServices: IEventoServices
+{
+    
+    private readonly IEventoRepository _eventoRepository;
+    public EventoServices(IEventoRepository eventoRepository)
+    {
+        _eventoRepository = eventoRepository;
+   
+    }
+
+    public void AgregarEvento(string nombre, string direccion, string provincia, string localidad, string numero, string hora, int idUsuarioCreador, int idDeporte, DateTime fecha)
+    {
+       Evento evento= new Evento();
+        evento.Nombre=nombre;
+        evento.Direccion=direccion;
+        evento.Provincia=provincia;
+        evento.Localidad=localidad;
+        evento.Numero=numero;
+        evento.Hora=hora;
+        evento.IdDeporte=idDeporte;
+        evento.IdUsuarioCreador=idUsuarioCreador;
+        evento.Fecha = new DateTime(fecha.Year, fecha.Month, fecha.Day);
+        evento.Finalizado = false;
+        _eventoRepository.AgregarEvento(evento);
+    }
+}
---
+++ Deportes.api/Controllers/Dto/DtoEvento.cs
@@ -0,0 +1,21 @@
+﻿namespace Deportes.api.Controllers.Dto
+{
+    public class DtoEvento
+    {
+        public string Nombre { get; set; } = null!;
+
+        public string Provincia { get; set; } = null!;
+
+        public string Localidad { get; set; } = null!;
+
+        public string Direccion { get; set; } = null!;
+
+        public string Numero { get; set; } = null!;
+        public string Hora { get; set; } = null!;
+        public int IdUsuarioCreador { get; set; }
+
+        public int IdDeporte { get; set; }
+
+        public DateTime Fecha { get; set; }
+    }
+}
